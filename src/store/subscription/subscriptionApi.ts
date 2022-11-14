e0e98e0d00d824eac9db3690c3988fbbc874bc05
Anastasia Brikun <brikun.anastasya@yandex.ru>
2022-11-14 18:42:19 +0400

    fix: fix url for deploy

--- src/store/subscription/subscriptionApi.ts
+++ src/store/subscription/subscriptionApi.ts
@@ -4,7 +4,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
 export const subscriptionApi = createApi({
     reducerPath: 'subscriptionApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://127.0.0.1:5000/api/v1/'
+        baseUrl: 'https://aviasales-app-subscribe.herokuapp.com/api/v1/'
     }),
     endpoints: (builder) => ({
         subscribeByEmail: builder.mutation<{id: number, email: string}, {email: string}>({
