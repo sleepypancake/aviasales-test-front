91ab0d525334b949dff61554c82275a08373ae16
Anastasia Brikun <brikun.anastasya@yandex.ru>
2022-11-15 12:20:35 +0400

    fix: text changes

--- src/entities/SubscriptionTitle/SubscriptionTitle.data.ts
+++ src/entities/SubscriptionTitle/SubscriptionTitle.data.ts
@@ -1,6 +1,6 @@
 export const UNSUBSCRIBED_TITLE_FIRST = 'все круто! теперь'
 export const UNSUBSCRIBED_TITLE_SECOND = 'выигрывай путешествие'
-export const UNSUBSCRIBED_TEXT = 'Чтобы участвовать в  розыгрыше путешествия, оставь актуальную почту и&nbsp;поделись с друзьями'
+export const UNSUBSCRIBED_TEXT = 'Чтобы участвовать в  розыгрыше путешествия, оставь актуальную почту и поделись с друзьями'
 
 export const SUBSCRIBED_TITLE_FIRST = 'класс! теперь ты'
 export const SUBSCRIBED_TITLE_SECOND = 'участвуешь в конкурсе'
